a104c080fb5b472fbfce6f6b322c08d6a2fcd037
manhcaoo <caotienmanh95@gmail.com>
2021-10-11 15:06:16 +0700

    done

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function clickButton() {
+  let input = document.getElementById("vnd").value;
+  let select1 = document.getElementById("select-1").value;
+  let select2 = document.getElementById("select-2").value;
+  alert(select1);
+}
